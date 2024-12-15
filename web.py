803695a0db1fdba42d5cfa1d0a8055e7346e58f8
Thorsten Claus <thorstenclaus@web.de>
2024-12-15 22:09:57 +0000

    initialies Commit

---
+++ web.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import detector as detector
+
+app = Flask(__name__)
+
+@app.route('/check', methods=["POST"])
+def handle_request():
+  print("Handle Request")
+  prompt = request.form.get("prompt")
+  if not prompt:
+    return {"error" : "prompt parameter required" }, 400
+  detection = detector.detect(prompt)
+  return {"spam": bool(detection)}
+
+#app.run(host = "0.0.0.0", port=8000)
